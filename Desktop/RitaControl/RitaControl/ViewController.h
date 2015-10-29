a94415325bfb5b97ba2a50e95ab22883f2308afd
PeremotovaYablonskaya <fpmi@forte01.local>
2015-10-29 18:30:44 +0300

    init

---
+++ Desktop/RitaControl/RitaControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RitaControl
+//
+//  Created by fpmi on 29.10.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
